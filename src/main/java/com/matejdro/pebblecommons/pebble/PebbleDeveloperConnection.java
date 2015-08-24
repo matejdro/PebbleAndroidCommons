935a90055fb17ffd2f56639e7b8fe28862346be7
Matej <matejdro@gmail.com>
2015-08-24 20:04:36 +0200

    Cleanup

--- src/main/java/com/matejdro/pebblecommons/pebble/PebbleDeveloperConnection.java
+++ src/main/java/com/matejdro/pebblecommons/pebble/PebbleDeveloperConnection.java
@@ -296,16 +296,6 @@ public class PebbleDeveloperConnection extends WebSocketClient
 		send(message);
 	}
 
-	public static byte[] hexStringToByteArray(String s) {
-		int len = s.length();
-		byte[] data = new byte[len / 2];
-		for (int i = 0; i < len; i += 2) {
-			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-					+ Character.digit(s.charAt(i+1), 16));
-		}
-		return data;
-	}
-
 	private void completeWaitingTasks(DeveloperConnectionTaskType type, Object result)
 	{
 		Iterator<DeveloperConnectionResult> iterator = waitingTasks.iterator();
