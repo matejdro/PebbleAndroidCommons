8e7156cfc81fba8d47048fcbdb484ce8082d558d
Matej Drobnič <matejdro@gmail.com>
2016-08-20 11:57:04 +0200

    Moved vibration pattern to PebbleAndroidCommons.

---
+++ src/main/java/com/matejdro/pebblecommons/pebble/PebbleVibrationPattern.java
@@ -0,0 +1,98 @@
+package com.matejdro.pebblecommons.pebble;
+
+import com.matejdro.pebblecommons.util.TextUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PebbleVibrationPattern
+{
+    public static List<Byte> parseVibrationPattern(String pattern)
+    {
+        String split[] = pattern.split(",");
+
+        List<Byte> bytes = new ArrayList<>(40);
+        int max = Math.min(20, split.length);
+        int total = 0;
+
+        for (int i = 0; i < max; i++)
+        {
+            try
+            {
+                int segment = Integer.parseInt(split[i].trim());
+                segment = Math.min(segment, 10000 - total);
+                total += segment;
+
+                bytes.add((byte) (segment & 0xFF));
+                bytes.add((byte) ((segment >> 8) & 0xFF));
+
+                if (total >= 10000)
+                    break;
+
+            } catch (NumberFormatException e)
+            {
+            }
+        }
+
+        if (bytes.size() == 0)
+        {
+            bytes.add((byte) 0);
+            bytes.add((byte) 0);
+        }
+
+        return bytes;
+    }
+
+    public static List<Byte> getFromAndroidVibrationPattern(long[] pattern)
+    {
+        List<Byte> bytes = new ArrayList<>(40);
+        int max = Math.min(20, pattern.length);
+        int total = 0;
+
+        //Android pattern has one extra pause, so we start at first vibration (pause,vib,pause,vib... instead of vib,pause,vib,pause...)
+        for (int i = 1; i < max; i++)
+        {
+            long segment = pattern[i];
+            segment = Math.min(segment, 10000 - total);
+            total += segment;
+
+            bytes.add((byte) (segment & 0xFF));
+            bytes.add((byte) ((segment >> 8) & 0xFF));
+
+            if (total >= 10000) //Maximum total vibration length is 10000 for now
+                break;
+        }
+
+        if (bytes.size() == 0)
+        {
+            bytes.add((byte) 0);
+            bytes.add((byte) 0);
+        }
+
+        return bytes;
+    }
+
+    public static boolean validateVibrationPattern(String pattern)
+    {
+        if (pattern.trim().isEmpty())
+            return false;
+
+        String split[] = pattern.split(",");
+
+        for (String s : split)
+        {
+            try
+            {
+                Integer number = Integer.parseInt(s.trim());
+                if (number < 0)
+                    return false;
+            }
+            catch (NumberFormatException e)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/com/matejdro/pebblecommons/pebble/PebbleVibrationPatternTest.java
@@ -0,0 +1,32 @@
+package com.matejdro.pebblecommons.pebble;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class PebbleVibrationPatternTest
+{
+    @Test
+    public void testValidation()
+    {
+        assertFalse(PebbleVibrationPattern.validateVibrationPattern(""));
+        assertFalse(PebbleVibrationPattern.validateVibrationPattern("a"));
+        assertFalse(PebbleVibrationPattern.validateVibrationPattern("1, b, 3"));
+        assertTrue(PebbleVibrationPattern.validateVibrationPattern("1, 2, 3"));
+        assertTrue(PebbleVibrationPattern.validateVibrationPattern("100, 50, 1000"));
+        assertFalse(PebbleVibrationPattern.validateVibrationPattern("-100, 50, 1000"));
+    }
+
+    @Test
+    public void testParsing()
+    {
+        assertArrayEquals(new Byte[] { 0, 0 }, PebbleVibrationPattern.parseVibrationPattern("").toArray(new Byte[0]));
+        assertArrayEquals(new Byte[] { 10, 0, 10, 0 }, PebbleVibrationPattern.parseVibrationPattern("10, 10").toArray(new Byte[0]));
+        assertArrayEquals(new Byte[] { 0x10, 0x27 }, PebbleVibrationPattern.parseVibrationPattern("20000, 5000, 100, 20").toArray(new Byte[0]));
+        assertArrayEquals(
+                new Byte[] { 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 10, 0, 11, 0, 12, 0, 13, 0, 14, 0, 15, 0, 16, 0, 17, 0, 18, 0, 19, 0, 20, 0 },
+                PebbleVibrationPattern.parseVibrationPattern("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24").toArray(new Byte[0]));
+    }
+}
